f9c23c40900e5dc3a478e0848f345d734f1a7962
Onur8817 <adilonurkarahann@gmail.com>
2024-08-28 15:00:09 +0300

    upd

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,7 +15,58 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: SafeArea(
           child: Column(
-        children: [],
+        children: [
+          Padding(
+            padding: EdgeInsets.only(left: 13, right: 13, top: 17),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Image.asset(
+                      "assets/images/logo.png",
+                      width: 104,
+                      height: 30,
+                    ),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Icon(
+                      Icons.keyboard_arrow_down_rounded,
+                      size: 16,
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    SvgPicture.asset(
+                      "assets/icons/kalp.svg",
+                      width: 24,
+                      height: 24,
+                    ),
+                    SizedBox(
+                      width: 24,
+                    ),
+                    SvgPicture.asset(
+                      "assets/icons/mesaj.svg",
+                      width: 24,
+                      height: 24,
+                    ),
+                    SizedBox(
+                      width: 24,
+                    ),
+                    SvgPicture.asset(
+                      "assets/icons/arti.svg",
+                      width: 24,
+                      height: 24,
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
       )),
     );
   }
